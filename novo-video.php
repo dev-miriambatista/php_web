e19b2cc7c2a6ffa2ea7af4102cfc32b9eeabb534
Miriam Batista <dev.miriambatista@gmail.com>
2024-07-25 09:10:39 -0300

    commit Criação de método inserir

---
+++ novo-video.php
@@ -0,0 +1,16 @@
+<?php
+
+$dbPath = __DIR__ . '/banco.sqlite';
+$pdo =new PDO("sqlite:$dbPath");
+
+$sql ='INSERT INTO videos (url, title) VALUES (?, ?)';
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(1, $_POST['url']);
+$stmt->bindValue(2, $_POST['titulo']);
+
+if($stmt->execute() === false){
+    header('location: index.php?sucesso=0');
+}else{
+    header('location: index.php?sucesso=1');
+}
+
