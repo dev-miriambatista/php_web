32c67f9f3f533cd8545d02fd6ea1ac4be2a665a5
Miriam Batista <dev.miriambatista@gmail.com>
2024-07-25 10:38:27 -0300

    commit Criação de filtro para url e titulo

--- novo-video.php
+++ novo-video.php
@@ -3,8 +3,23 @@
 $dbPath = __DIR__ . '/banco.sqlite';
 $pdo =new PDO("sqlite:$dbPath");
 
+$url = filter_input(INPUT_POST, 'url', FILTER_SANITIZE_URL);
+if ($url === false) {
+    header("Location: index.php?sucesso=0");
+    exit();
+}
+
+$titulo = filter_input(INPUT_POST, 'titulo', FILTER_SANITIZE_STRING);
+if ($titulo === false) {
+    header("Location: index.php?sucesso=0");
+    exit();
+
+}
+
 $sql ='INSERT INTO videos (url, title) VALUES (?, ?)';
+
 $stmt = $pdo->prepare($sql);
+
 $stmt->bindValue(1, $_POST['url']);
 $stmt->bindValue(2, $_POST['titulo']);
 
