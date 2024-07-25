2f11159bb44fbf78afbfbe82486e32ec018d2e5d
Miriam Batista <dev.miriambatista@gmail.com>
2024-07-25 11:15:19 -0300

    commit Criação de remoção de video

---
+++ remover-video.php
@@ -0,0 +1,15 @@
+<?php
+
+$dbPath = __DIR__ . '/banco.sqlite';
+$pdo =new PDO("sqlite:$dbPath");
+
+$id = $_GET['id'];
+$sql = 'DELETE FROM videos WHERE id = ?';
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(1,$id);
+
+if($stmt->execute() === false) {
+    header('location: index.php?sucesso=0');
+}else{
+    header('location:index.php?sucesso=1');
+    }
